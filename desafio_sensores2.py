992820fb2e75545d6d5932fdedf54cfaf683d645
maria <me15degrees@gmail.com>
2024-02-04 18:14:06 -0300

    adicionando comentários

--- desafio_sensores2.py
+++ desafio_sensores2.py
@@ -3,6 +3,11 @@ from pybricks.ev3devices import Motor
 from pybricks.parameters import Port
 from pybricks.tools import wait
 
+"""
+Para fazer um quadrado, existem dois movimentos básicos: andar para frente e virar.
+Separei esses movimentos em duas funções e usei como parâmetro para a função a velocidade e o tempo de espera para cada um deles de acordo com o código fonte.
+"""
+
 def mover_frente(velocidade, tempo):
     motor_esquerdo.run(speed=velocidade)
     motor_direito.run(speed=velocidade)
